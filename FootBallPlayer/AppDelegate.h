e1b5991291fcc612c410eeb31a53dc9fa76ea7ca
anhminh115 <anh_minh11590@yahoo.com>
2013-04-17 21:32:36 +0700

    Initial Commit

---
+++ FootBallPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FootBallPlayer
+//
+//  Created by Anh Minh on 4/17/13.
+//  Copyright (c) 2013 FPT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
